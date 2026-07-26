101902dacb9818f38e4c083e613142c1e56c97b6
Hajar GTC <hajarazlafi@gmail.com>
2026-07-26 12:11:31 +0100

    Initialize Spring Boot backend

---
+++ campusops-backend/src/main/java/com/campusops/CampusopsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.campusops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusopsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusopsBackendApplication.class, args);
+	}
+
+}
